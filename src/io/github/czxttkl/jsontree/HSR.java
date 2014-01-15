b2ab3d1a2114ecfe124e6ca5f000a366c225b419
czxttkl <czxttkl@gmail.com>
2014-01-15 00:16:50 -0500

    replace hsr main java

--- src/io/github/czxttkl/jsontree/HSR.java
+++ src/io/github/czxttkl/jsontree/HSR.java
@@ -119,37 +119,99 @@ public class HSR {
 		
 	}
 
-	
-	public static void checkInternalNode(HSRTree root1, int n) {
+	public static void checkLeftSubtree(Node node, int n) {
+		int max = findLargest(node);
+		leftSubTreeValidated = n - max == 1 ? true : false;
+		System.out.println("");
+		System.out.println("Check if the root is one larger than the largest node in the left subtree: " + (leftSubTreeValidated ? "yes" : "no"));
+		System.out.println("The root is " + n + ". The largest node in the left subtree is " + max);
+	}
+
+	public static int findLargest(Node o) {
+		if (o instanceof Leaf)
+			return ((Leaf) o).getH();
+		else {
+			return Math.max(findLargest(((HSRTree) o).getLeftNode()), findLargest(((HSRTree) o).getRightNode()));
+		}
+	}
+
+	public static void checkRightSubtree(Node node, int n) {
+		int min = findSmallest(node);
+		rightSubTreeValidated = n == min ? true : false;
+		System.out.println("");
+		System.out.println("Check if the root is equal to the smallest node in the right subtree: " + (rightSubTreeValidated ? "yes" : "no"));
+		System.out.println("The root is " + n + ". The smallest node in the right subtree is " + min);
+	}
+
+	public static int findSmallest(Node o) {
+		if (o instanceof Leaf)
+			return ((Leaf) o).getH();
+		else {
+			return Math.min(findSmallest(((HSRTree) o).getLeftNode()), findSmallest(((HSRTree) o).getRightNode()));
+		}
+	}
+
+	public static void checkQEdges(HSRTree root, int q) {
+		int maxDepth = maxDepth(root);
+		if (q >= maxDepth)
+			qEdgesValidated = true;
+		System.out.println("");
+		System.out.println("Check if the longest root-leaf path has " + q + " edges: " + (q >= maxDepth ? "yes" : "no"));
+		System.out.println("The max depth of the tree: " + maxDepth);
+	}
+
+	public static int maxDepth(Node node) {
+		if (node instanceof Leaf)
+			return 0;
+		if (node instanceof HSRTree)
+			return Math.max(maxDepth(((HSRTree) node).getLeftNode()), maxDepth(((HSRTree) node).getRightNode())) + 1;
+		return -1;
+	}
+
+	public static void checkKYes(HSRTree root, int k) {
+		int maxLeftEdges = maxLeftEdges(root, 0);
+		if (maxLeftEdges <= k)
+			kYesValidated = true;
+		System.out.println("");
+		System.out.println("Check if there are at most " + k + " yes from the root to any leaf: " + (kYesValidated ? "yes" : "no"));
+		System.out.println("The maximum of left edges in the tree is " + maxLeftEdges);
+	}
+
+	/**
+	 * @param side
+	 *            1: left child of its parent; -1: right child of its parent; 0: no parent;
+	 */
+	public static int maxLeftEdges(Node node, int side) {
+		if (node instanceof Leaf) {
+			return 0;
+		} else {
+			return Math.max(maxLeftEdges(((HSRTree) node).getLeftNode(), 1) + 1, maxLeftEdges(((HSRTree) node).getRightNode(), -1));
+		}
+	}
+
+	public static void checkInternalNode(HSRTree root, int n) {
 		int[] record = new int[n];
 		Arrays.fill(record, 0);
 		System.out.println("");
 		System.out.print("Check if each rung 1..n-1 appears exactly once as internal node: ");
 		try {
-			record = recordInternalNode(root1.decision_tree[1], record);
-			record = recordInternalNode(root1.decision_tree[2], record);
-			record[root1.getRung()]++;
-			
-			for ( int i = 1; i < n; i++) {
+			record = recordInternalNode(root, record);
+			for (int i = 1; i < n; i++) {
 				int j = record[i];
-
 				if (j != 1) {
-					if ( j > 1)
+					if (j > 1)
 						throw new DuplicateException(i);
 					else
 						throw new NotAppearException(i);
 				}
-				
 			}
-			
 			System.out.println("yes");
 			internalNodeValidated = true;
-			
 		} catch (OutBoundException e) {
 			System.out.println("no");
-			if (e.outIndex > (n-1)) 
-				System.out.println("Exists a node with value " + e.outIndex + " that is larger than " + (n-1));
-			else 
+			if (e.outIndex > (n - 1))
+				System.out.println("Exists a node with value " + e.outIndex + " that is larger than " + (n - 1));
+			else
 				System.out.println("Exists a node with value " + e.outIndex + " that is smaller than 0");
 		} catch (DuplicateException e) {
 			System.out.println("no");
@@ -157,36 +219,31 @@ public class HSR {
 		} catch (NotAppearException e) {
 			System.out.println("no");
 			System.out.println("Rung " + e.noIndex + " didn't show up as a internal node.");
-		} 
+		}
 	}
-	
-	
-	public static int[] recordInternalNode(Object o, int[] record) throws OutBoundException {
-		if(o instanceof LinkedTreeMap)
+
+	public static int[] recordInternalNode(Node node, int[] record) throws OutBoundException {
+		if (node instanceof Leaf) {
 			return record;
-		
-		if(o instanceof ArrayList) {
-			int i = ((Double) ((ArrayList)o).get(0)).intValue();
-			if( i <= record.length-1 && i>=1)
+		} else {
+			int i = ((HSRTree) node).getRung();
+			if (i <= record.length - 1 && i >= 1)
 				record[i]++;
 			else
 				throw new OutBoundException(i);
-			record = recordInternalNode( ((ArrayList)o).get(1), record);
-			record = recordInternalNode( ((ArrayList)o).get(2), record);
+			record = recordInternalNode(((HSRTree) node).getLeftNode(), record);
+			record = recordInternalNode(((HSRTree) node).getRightNode(), record);
+			return record;
 		}
-		
-		return record;
 	}
 	
-	public static void checkLeaf(HSRTree root1, int n) {
+	public static void checkLeaf(HSRTree root, int n) {
 		int[] record = new int[n];
 		Arrays.fill(record, 0);
 		System.out.println("");
 		System.out.print("Check if each rung 0..n-1 appears exactly once as a leaf: ");
 		try {
-			record = recordLeaf(root1.decision_tree[1], record);
-			record = recordLeaf(root1.decision_tree[2], record);
-			
+			record = recordLeaf(root, record);
 			for(int p = 0; p < n; p++) {
 				int q = record[p];
 				if ( q!=1 ) {
@@ -196,10 +253,8 @@ public class HSR {
 						throw new NotAppearException(p);
 				}
 			}
-			
 			System.out.println("yes");
 			leafValidated = true;
-			
 		} catch (OutBoundException e) {
 			System.out.println("no");
 			if(e.outIndex > (n-1)){				
@@ -216,429 +271,21 @@ public class HSR {
 		}
 	}
 	
-	public static int[] recordLeaf(Object o, int[] record) throws OutBoundException {
-		Stack s = new Stack();
-
-		Object prev = null;
-		Object curr = o;
-		s.push(curr);
-
-		do {
-
-			if (curr instanceof LinkedTreeMap) {
-				Double h = (Double) ((LinkedTreeMap) curr).get("h");
-				int intH = h.intValue();
-				
-				if(intH <= record.length-1 && intH >=0) {
-					record[intH]++;
-				}
-				else {
-					OutBoundException e = new OutBoundException(intH);
-					throw e;
-				}
-				
-				//if prev==null then the subtree is only one leaf
-				if(prev!=null) {
-					prev = curr;
-				 	curr = s.pop();
-				 	continue;
-				}else
-					break;
+	public static int[] recordLeaf(Node node, int[] record) throws OutBoundException {
+		if (node instanceof Leaf) {
+			int h = ((Leaf)node).getH();
+			if(h <= record.length-1 && h >=0) {
+				record[h]++;
+			} else {
+				throw new OutBoundException(h);
 			}
-
-			if (curr instanceof ArrayList) {
-
-				if (prev == null) {
-					prev = curr;
-					curr = ((ArrayList) curr).get(1);
-					continue;
-				}
-
-				if (prev instanceof LinkedTreeMap) {
-					if (prev == ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-					if (prev == ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-				} 
-				
-				if (prev instanceof ArrayList) {
-
-					if (curr == ((ArrayList) prev).get(2)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-
-					if (curr == ((ArrayList) prev).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-				}
-
-			}
-		} while (!s.empty()
-				|| (curr instanceof ArrayList ? (prev != ((ArrayList) curr)
-						.get(2)) : true));
-
-		return record;
+			return record;
+		} else {
+			int i = ((HSRTree) node).getRung();
+			record = recordLeaf(((HSRTree) node).getLeftNode(), record);
+			record = recordLeaf(((HSRTree) node).getRightNode(), record);
+			return record;
+		}
 	}
-	
-	
-	public static void checkRightSubtree(Object object, int n) {
-		int min = findSmallest(object);
-		System.out.println("");
-		System.out.println("Check if the root is equal to the smallest node in the right subtree: " + (n==min?"yes":"no"));
-		if( n == min )
-			rightSubTreeValidated = true;
-		System.out.println("The root is " + n + ". The smallest node in the right subtree is " + min);
-	}
-	
-	
-	public static void checkLeftSubtree(Object object, int n) {
-		int max = findLargest(object);
-		System.out.println("");
-		System.out.println("Check if the root is one larger than the largest node in the left subtree: " + (n>max?"yes":"no") );
-		if( n > max )
-			leftSubTreeValidated = true;
-		System.out.println("The root is " + n + ". The largest node in the left subtree is " + max);
-	}
-
-
-
-	public static int maxDepth(Object o) {
-		if (o instanceof LinkedTreeMap)
-			return 0;
-		if (o instanceof HSRTree)
-			return Math.max(maxDepth(((HSRTree) o).decision_tree[1]),
-					maxDepth(((HSRTree) o).decision_tree[2])) + 1;
-
-		// Then o must be arraylist
-		ArrayList oa = (ArrayList) o;
-
-		int leftMax = maxDepth(oa.get(1));
-		int rightMax = maxDepth(oa.get(2));
-
-		return (leftMax > rightMax) ? leftMax + 1 : rightMax + 1;
-	}
-
-	
-	
-	
-	public static void checkQEdges(HSRTree root, int q) {
-		int maxDepth = maxDepth(root);
-		System.out.println("");
-		System.out.println("Check if the longest root-leaf path has " + q
-				+ " edges: " + ( q >= maxDepth ? "yes" : "no"));
-		if( q >= maxDepth )
-			qEdgesValidated = true;
-		System.out.println("The max depth of the tree: " + maxDepth);
-	}
-
-	
-	
-	public static int findLargest(Object o) {
-		int max = 0;
-		Stack s = new Stack();
-
-		Object prev = null;
-		Object curr = o;
-		s.push(curr);
-
-		do {
-
-			if (curr instanceof LinkedTreeMap) {
-				Double h = (Double) ((LinkedTreeMap) curr).get("h");
-				if (h.intValue() > max) {
-					max = h.intValue();
-				}
-				
-				//if prev==null then the subtree is only one leaf
-				if(prev!=null) {
-					prev = curr;
-				 	curr = s.pop();
-				 	continue;
-				}else
-					break;
-			}
-
-			if (curr instanceof ArrayList) {
-
-				if (prev == null) {
-					prev = curr;
-					curr = ((ArrayList) curr).get(1);
-					continue;
-				}
-
-				if (prev instanceof LinkedTreeMap) {
-					if (prev == ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-					if (prev == ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-				} 
-				
-				if (prev instanceof ArrayList) {
-
-					if (curr == ((ArrayList) prev).get(2)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-
-					if (curr == ((ArrayList) prev).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-				}
-
-			}
-		} while (!s.empty()
-				|| (curr instanceof ArrayList ? (prev != ((ArrayList) curr)
-						.get(2)) : true));
-
-		return max;
-	}
-
-	public static void checkKYes(HSRTree root, int k) {
-		int leftTree = maxLeftEdges(root.decision_tree[1]) + 1;
-		int rightTree = maxLeftEdges(root.decision_tree[2]);
-		int maxLeftEdges = Math.max(leftTree, rightTree);
-		System.out.println("");
-		System.out.println("Check if there are at most " + k + " yes from the root to any leaf: " + (maxLeftEdges <= k? "yes":"no"));
-		if ( maxLeftEdges <= k )
-			kYesValidated = true;
-		System.out.println("The maximum of left edges in the tree is " + maxLeftEdges);
-	}
-	
-	public static int maxLeftEdges(Object o) {
-		int maxEdges = 0;
-		int edges = 0;
-		Stack s = new Stack();
-		Object prev = null;
-		Object curr = o;
-		s.push(curr);
-
-		do {
-
-			if (curr instanceof LinkedTreeMap) {
-				//if prev==null then the subtree is only one leaf
-				if(prev!=null) {
-					prev = curr;
-				 	curr = s.pop();
-				 	edges--;
-				 	continue;
-				}else
-					break;
-			}
-
-			if (curr instanceof ArrayList) {
-
-				if (prev == null) {
-					prev = curr;
-					curr = ((ArrayList) curr).get(1);
-					edges++;
-					if(edges > maxEdges)
-						maxEdges = edges;
-					continue;
-				}
-
-				if (prev instanceof LinkedTreeMap) {
-					if (prev == ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						edges--;
-						continue;
-					}
-
-					if (prev == ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-				} 
-				
-				if (prev instanceof ArrayList) {
-
-					if (curr == ((ArrayList) prev).get(2)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-
-					if (curr == ((ArrayList) prev).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						edges++;
-						if(edges > maxEdges)
-							maxEdges = edges;
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						edges--;
-						continue;
-					}
-
-				}
-
-			}
-		} while (!s.empty()
-				|| (curr instanceof ArrayList ? (prev != ((ArrayList) curr)
-						.get(2)) : true));
-
-		return maxEdges;
-	}
-	
-	
-	
-	public static int findSmallest(Object o) {
-		int min = 2147483647;
-		Stack s = new Stack();
-
-		Object prev = null;
-		Object curr = o;
-		s.push(curr);
-
-		do {
-
-			if (curr instanceof LinkedTreeMap) {
-				Double h = (Double) ((LinkedTreeMap) curr).get("h");
-				if (h.intValue() < min) {
-					min = h.intValue();
-				}
-				
-				//if prev==null then the subtree is only one leaf
-				if(prev!=null) {
-					prev = curr;
-				 	curr = s.pop();
-				 	continue;
-				}else
-					break;
-			}
-
-			if (curr instanceof ArrayList) {
-
-				if (prev == null) {
-					prev = curr;
-					curr = ((ArrayList) curr).get(1);
-					continue;
-				}
-
-				if (prev instanceof LinkedTreeMap) {
-					if (prev == ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-					if (prev == ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-				} 
-				
-				if (prev instanceof ArrayList) {
-
-					if (curr == ((ArrayList) prev).get(2)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-
-					if (curr == ((ArrayList) prev).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(1);
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(2)) {
-						prev = curr;
-						curr = s.pop();
-						continue;
-					}
-					
-					if(prev== ((ArrayList) curr).get(1)) {
-						s.push(curr);
-						prev = curr;
-						curr = ((ArrayList) curr).get(2);
-						continue;
-					}
-
-				}
-
-			}
-		} while (!s.empty()
-				|| (curr instanceof ArrayList ? (prev != ((ArrayList) curr)
-						.get(2)) : true));
-
-		return min;
-	}
-	
-	
 	
 }
